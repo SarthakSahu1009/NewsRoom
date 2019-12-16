b2b4189a22920dde78ac623d458a4d2ba9bc6f5c
SarthakSahu1009 <sarthaksashu1009@gmail.com>
2019-12-16 21:58:38 +0530

    Completed frontend and made local host server

---
+++ DevFolio/app .js
@@ -0,0 +1,18 @@
+var express = reqire('express');
+
+var app = express();
+
+app.get('/',function(req,res)
+{
+    res.sendfile('login.html');
+
+})
+app.get('/welcome',function(req,res)
+{
+    res.sendfile('welcome.html');
+})
+
+app.listen(3001,()=>
+   {
+       console.log("server is working");
+   })
